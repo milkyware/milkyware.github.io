9352864e5945ae453e78a6e8c3687877e0bd1cef
Cameron Cowen <cameronc94@gmail.com>
2023-05-19 21:27:36 +0000

    added theme mapping

--- assets/js/mermaid.js
+++ assets/js/mermaid.js
@@ -1,7 +1,23 @@
+---
+---
 $(document).ready(function () {
-    mermaid.initialize({
+    var skin = "{{ site.minimal_mistakes_skin }}"
+    var theme = {
+      "air": "default",
+      "aqua": "default",
+      "contrast": "default",
+      "dark": "dark",
+      "default": "default",
+      "dirt": "default",
+      "mint": "mint",
+      "neon": "dark",
+      "plum": "dark",
+      "sunrise": "default"
+    }[skin]
+    var config = {
       startOnLoad:true,
-      theme: "default",
-    });
-    window.mermaid.init(undefined, document.querySelectorAll('.language-mermaid'));
+      theme: theme,
+    }
+    mermaid.initialize(config)
+    window.mermaid.init(config, document.querySelectorAll('.language-mermaid'));
   });

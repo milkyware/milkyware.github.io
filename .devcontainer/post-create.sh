199b6872fc2285eb726cecd208253bb74fec344e
Cameron Cowen <cameronc94@gmail.com>
2024-06-23 14:30:37 +0000

    formatting

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
 # Install the version of Bundler.
-if [ -f Gemfile.lock ] && grep "BUNDLED WITH" Gemfile.lock > /dev/null; then
+if [ -f Gemfile.lock ] && grep "BUNDLED WITH" Gemfile.lock > /dev/null
+then
     cat Gemfile.lock | tail -n 2 | grep -C2 "BUNDLED WITH" | tail -n 1 | xargs gem install bundler -v
 fi
 
 # If there's a Gemfile, then run `bundle install`
 # It's assumed that the Gemfile will install Jekyll too
-if [ -f Gemfile ]; then
+if [ -f Gemfile ]
+then
     bundle install
 fi
 
 # Install PowerShell Modules
-pwsh -Command Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-pwsh -Command Install-Module posh-git
-pwsh -Command Add-PoshGitToProfile
-pwsh -Command Install-Module psreadline
+pwsh -c "Set-PSRepository -Name PSGallery -InstallationPolicy Trusted"
+pwsh -c "Install-Module posh-git"
+pwsh -c "Add-PoshGitToProfile"
+pwsh -c "Install-Module psreadline"
